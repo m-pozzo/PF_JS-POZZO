f0d9ac453998ae84109f2634447be6059b0dbb87
Martin Pozzo <martinpozzo.tin@gmail.com>
2023-06-07 22:45:05 -0300

    Subida a Github

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let distancia = parseInt(prompt("Decime la distancia total que hay entre el lugar de salida y de destino"));
+let tiempo = parseInt(prompt("Decime el tiempo que tenes estimado tardar"));
+let velocidad = parseInt(prompt("Decime a la velocidad que vas (m/s)"))
+
+
