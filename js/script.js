cba41c4e1d8a54b3b4b30e3c3c713ecf1969b01f
Martin Pozzo <martinpozzo.tin@gmail.com>
2023-06-29 23:28:53 -0300

    simulador pedir comida

--- js/script.js
+++ js/script.js
@@ -1,50 +1,42 @@
-alert("Bienvenido! \n Esto es una calculadora para calcular:\n-Velocidad a la que vas (m/s) \n-Distancia (metros) que recorriste en base al tiempo y a la velocidad que vas\n-Tiempo (segundos) dependiendo a que velocidad recorriste cierta distancia");
+alert("Bienvenido! Esto es un simulador de restaurante");
+let cantidadPersonas = parseInt(prompt("Venis a comer solo o con gente? (Máximo de cuatro personas)\nHacemelo saber poniendo un numero\nEj: 2."));
+alert("Perfecto. El menu es el siguiente:\n-empanada $180\n-milanesa $1100\n-ensalada $550\n-donas $150");
 
-let eleccion = "";
-let opcionValida = false;
 
+let total = 0;
+let plato = "";
+const comida = [];
 
-while (!opcionValida) {
-    let eleccion = prompt("-'V' para calcular velocidad\n-'D' para calcular distancia\n-'T' para calcular tiempo\n-'S' o 'salir' para terminar de usar la calculadora");
+const menu = {
+    "empanada": 180,
+    "milanesa": 1100,
+    "ensalada": 550,
+    "donas": 150
+};
 
-    if (eleccion === "V" || eleccion === "v" || eleccion === "velocidad" || eleccion === "Velocidad") {
-        calcularVelocidad();
-        opcionValida = false;
-    } else if (eleccion === "D" || eleccion === "d" || eleccion === "distancia" || eleccion === "Distancia") {
-        calcularDistancia();
-        opcionValida = false;
-    } else if (eleccion === "T" || eleccion === "t" || eleccion === "tiempo" || eleccion === "Tiempo") {
-        calcularTiempo();
-        opcionValida = false;
-    } else if (eleccion === "S" || eleccion === "s" || eleccion === "salir") {
-        alert("Espero que te haya gustado crack\nNos vemos en la proxima pre-entrega!!!")
-        opcionValida = true;
-    } else {
-        alert("No me ingreses cualquier cosa, no doy para tanto:v (por ahora)\nVamos a intentar de nuevo👍")
+function pedirComida() {
+    if (cantidadPersonas > 1) {
+        do {
+            plato = prompt("Que van a querer comer? (Ordenar uno a la vez por favor");
+            comida.push(plato);
+        } while (comida.length < cantidadPersonas && cantidadPersonas <= 4)
+    } else if (cantidadPersonas == 1) {
+        plato = prompt("Que vas a querer comer?");
+        comida.push(plato);
+    }
+    alert("Perfecto! El pedido ya está siendo preparado");
+    if (comida.length > 0) {
+        for (const comidaItem of comida) {
+            if (menu.hasOwnProperty(comidaItem)) {
+                total += menu[comidaItem.toLowerCase()];
+            }
+        }
+        if (cantidadPersonas > 1) {
+            alert("Espero que hayan disfrutado la comida.\nAcá está el total: $" + total);
+        }
+        else if (cantidadPersonas == 1){
+            alert("Espero que hayas disfrutado la comida.\nAcá está el total: $" + total);
+        }
     }
 }
-
-function calcularVelocidad() {
-    alert("Queres calcular tu velocidad? Perfecto, vamos a calcularlo");
-    let distancia = parseInt(prompt("Primero, necesito que me digas cuanta distancia recorriste"));
-    let tiempo = parseInt(prompt("Y por ultimo, necesito que me digas en cuanto tiempo recorriste esa distancia"));
-    let calculoVelocidad = distancia / tiempo;
-    alert(`Tu velocidad es de ${calculoVelocidad} m/s`);
-}
-
-function calcularDistancia() {
-    alert("Queres calcular la distancia que recorriste? Perfecto, vamos a calcularlo");
-    let velocidad = parseInt(prompt("Primero, necesito que me digas a que velocidad ibas"));
-    let tiempo = parseInt(prompt("Y por ultimo, necesito que me digas por cuanto tiempo fuiste a esa velocidad"));
-    let calculoDistancia = velocidad * tiempo;
-    alert(`El distancia que recorriste es de ${calculoDistancia} m`);
-}
-
-function calcularTiempo() {
-    alert("Queres calcular el tiempo que tardaste? Perfecto, vamos a calcularlo");
-    let distancia = parseInt(prompt("Primero, necesito que me digas cuanta distancia recorriste"));
-    let velocidad = parseInt(prompt("Y por ultimo, necesito que me digas a que velocidad ibas"));
-    let calculoTiempo = distancia / velocidad;
-    alert(`El tiempo que tardaste es de ${calculoTiempo} s`);
-}
-
+pedirComida();
