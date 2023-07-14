2b6f082673428075da82a35c4ec1b7f77e5f1928
Martin Pozzo <martinpozzo.tin@gmail.com>
2023-07-13 23:27:48 -0300

    carrito de compras hecho

--- js/script.js
+++ js/script.js
@@ -1,42 +1,107 @@
-alert("Bienvenido! Esto es un simulador de restaurante");
-let cantidadPersonas = parseInt(prompt("Venis a comer solo o con gente? (Máximo de cuatro personas)\nHacemelo saber poniendo un numero\nEj: 2."));
-alert("Perfecto. El menu es el siguiente:\n-empanada $180\n-milanesa $1100\n-ensalada $550\n-donas $150");
+let carrito = [];
 
-
-let total = 0;
-let plato = "";
-const comida = [];
-
-const menu = {
-    "empanada": 180,
-    "milanesa": 1100,
-    "ensalada": 550,
-    "donas": 150
-};
-
-function pedirComida() {
-    if (cantidadPersonas > 1) {
-        do {
-            plato = prompt("Que van a querer comer? (Ordenar uno a la vez por favor");
-            comida.push(plato);
-        } while (comida.length < cantidadPersonas && cantidadPersonas <= 4)
-    } else if (cantidadPersonas == 1) {
-        plato = prompt("Que vas a querer comer?");
-        comida.push(plato);
-    }
-    alert("Perfecto! El pedido ya está siendo preparado");
-    if (comida.length > 0) {
-        for (const comidaItem of comida) {
-            if (menu.hasOwnProperty(comidaItem)) {
-                total += menu[comidaItem.toLowerCase()];
-            }
-        }
-        if (cantidadPersonas > 1) {
-            alert("Espero que hayan disfrutado la comida.\nAcá está el total: $" + total);
-        }
-        else if (cantidadPersonas == 1){
-            alert("Espero que hayas disfrutado la comida.\nAcá está el total: $" + total);
-        }
+class Platos{
+    constructor(id, comida, precio, descripcion, imagen){
+        this.id = id;
+        this.comida = comida;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
     }
 }
-pedirComida();
+
+const platos = [
+    new Platos(1, 'hamburguesa', 1200, 'hamburguesa con papas bien grande', './img/hamburguesa.jfif'),
+    new Platos(2, 'tacos', 1600, 'tres tacos con un relleno riquisimo', './img/tacos.jfif'),
+    new Platos(3, 'ensalada de sushi', 1350, 'ensalada de sushi DEL ORIENTE', './img/sushi.jfif'),
+    new Platos(4, 'ratatuil', 950, 'ratauil colorido', './img/rata.jfif'),
+    new Platos(5, 'brochet de camarones', 800, 'brochet maritimo', './img/brochet.jfif'),
+    new Platos(6, 'picada fuerte', 5600, 'picada para 5 personas', './img/picada.jfif')
+];
+
+const plateContainer = document.querySelector('#plate-container');
+
+
+platos.forEach(plato => {
+    const platoDiv = document.createElement('div');
+    platoDiv.innerHTML = `
+    <article class="menu">
+        <div class="info-comida">
+            <img src="${plato.imagen}">
+            <h2>${plato.comida}</h2>
+            <p>$${plato.precio}</p>
+            <p>${plato.descripcion}</p>
+        </div>
+        <button class="boton-carrito">Agregar al carrito</button>
+    </article>
+    `;
+
+    plateContainer.appendChild(platoDiv);
+
+
+    const cartBtn = platoDiv.querySelector('.boton-carrito');
+    cartBtn.addEventListener('click', () => {
+
+        carrito.push(plato);
+        guardarCarrito(carrito); 
+        mostrarCarrito()
+    });
+})
+
+const verCartBtn = document.getElementById('ver-carrito');
+    
+    
+function guardarCarrito(carrito){
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function mostrarCarrito(){
+    const modalContainer = document.getElementById('modal-container');
+    // Esto se hace para ver si el carrito existe en el localStorage y si esta guardado
+    const carritoSaved = localStorage.getItem('carrito');
+
+    let totalPrice = 0;
+
+    if (carritoSaved){
+        const carritoParsed = JSON.parse(carritoSaved);
+        carritoParsed.forEach((plato) => {
+            const platoDiv = document.createElement('div');
+            platoDiv.innerHTML = `
+            <div class="flex-modal">
+            <img src="${plato.imagen}">
+                <div class="flex-modal-info">
+                    <h2>${plato.comida}</h2>
+                    <h3>${plato.descripcion}</h3>
+                    <p>$${plato.precio}</p>
+                </div>
+            </div>
+            <button class="delete">Eliminar del carrito</button>
+            `;
+
+            modalContainer.appendChild(platoDiv)
+            totalPrice += plato.precio;
+        })
+    }
+
+    const totalPriceElement = document.createElement('p');
+    totalPriceElement.textContent = `Precio total: $${totalPrice.toFixed(2)}`;
+    modalContainer.appendChild(totalPriceElement);
+    debugger
+    // delete plato
+    const deleteBtn = document.getElementsByClassName('delete');
+    deleteBtn.addEventListener('click', ()=> {
+        deletePlate(index);
+        platoDiv.remove();
+        totalPrice -= plato.precio;
+    });
+}
+
+
+
+    // const ulNav = document.querySelector('.ul-nav');
+    // const li = document.createElement('li');
+    // li.innerHTML = `
+    //     <li class="contador">${carrito.length}</li>
+    // `;
+    
+    // ulNav.appendChild(li);
